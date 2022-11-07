84e9c87a0ca9e858492fa286463e3b1d3b3d7726
Lud <swiknaba@users.noreply.github.com>
2022-11-07 16:27:03 +0700

    pass credentials to UI

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,13 @@
   windows_subsystem = "windows"
 )]
 
+mod aws_credential_loader;
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      aws_credential_loader::load_credentials,
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
