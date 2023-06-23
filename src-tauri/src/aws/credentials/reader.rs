6c04c33e58a9907e4d2b2bc45ef297ccaf43d4e4
morozRed <moroz.grigory@gmail.com>
2023-06-23 22:10:58 +0200

    fetch all profiles from credentials

--- src-tauri/src/aws/credentials/reader.rs
+++ src-tauri/src/aws/credentials/reader.rs
@@ -1,21 +1,30 @@
 extern crate dirs;
+extern crate ini;
 
 use crate::aws::credentials::{
   AWSCredential,
   credentials_path,
 };
 
+use ini::{Ini, Properties};
+use std::collections::HashMap;
 use regex::Regex;
 use std::fs;
 
-pub fn get_credentials() -> Vec<AWSCredential> {
-  let mut credentials:Vec<AWSCredential> = Vec::new();
+pub fn get_credentials() -> HashMap<String, AWSCredential> {
+  let mut credentials:HashMap<String, AWSCredential> = HashMap::new();
 
-  // @TODO(lud, 2022-11-07): parse all configs from "data" and push into vector
   let data = load_credentials_file();
-  let test = parse_credential(data);
-  let dummy_credential = build_aws_credential_template(); // for testing
-  credentials.push(dummy_credential);
+  print!("data: {:?}", data);
+
+  for (sec, props) in &data {
+    if sec.is_none() {
+      continue;
+    }
+
+    let profile_name = sec.unwrap().to_string();
+    credentials.insert(profile_name, parse_credential(sec, props.clone()));
+  }
 
   credentials
 }
@@ -32,50 +41,46 @@ fn build_aws_credential_template() -> AWSCredential {
   }
 }
 
-fn load_credentials_file() -> String {
+fn load_credentials_file() -> Ini {
   let path = credentials_path();
   let error_msg = String::from(
     "Error while reading credentials file from "
   ) + &path;
 
-  fs::read_to_string(path).expect(&error_msg)
+  let buf = fs::read_to_string(path).expect(&error_msg);
+  Ini::load_from_str(&buf).unwrap()
 }
 
 // @NOTE: this is totally broken and horrible. Better approach:
 //        iterate over every line, start a new profile once we hit a row that matches "[.*]"
 //        and then parse the key/value pairs from the following rows, filling default values if missing
-fn parse_credential(raw: String) -> AWSCredential {
-  let template = build_aws_credential_template();
-  let profile_name = String::from("default");
+fn parse_credential(sec: Option<&str>, props: Properties) -> AWSCredential {
+  let re = Regex::new(r"^aws_(.+)").unwrap();
+  let sec = sec.unwrap().to_string();
 
-  // @NOTE: the regex only works if there is **some** char after the last value, e.g. a newline
-  //        this especially might be a problem for the last line/entry of that profile.
-  //        We want a non-greedy matcher to not match the optional quotes, but still have to stop at some point.
-  let profile_name_regex = Regex::new(r#"\[(.*)\]"#).unwrap();
-  let aws_access_key_id_regex = Regex::new(r#"aws_access_key_id\s{0,}=\s{0,}(.*?)\[\n\r\s]{1}"#).unwrap();
-  let aws_secret_access_key_regex = Regex::new(r#"aws_secret_access_key\s{0,}=\s{0,}(.*?)\[\n\r\s]{1}"#).unwrap();
-  let region_regex = Regex::new(r#"region\s{0,}=\s{0,}(.*?)\[\n\r\s]{1}"#).unwrap();
-  let output_regex = Regex::new(r#"output\s{0,}=\s{0,}(.*?)\[\n\r\s]{1}"#).unwrap();
+  let mut aws_access_key_id = "".to_string();
+  let mut aws_secret_access_key = "".to_string();
+  let mut region = None;
+  let mut output = None;
 
+  for (key, value) in props.iter() {
+    if let Some(caps) = re.captures(key) {
+      let key = caps.get(1).map_or("", |m| m.as_str());
+      match key {
+        "access_key_id" => aws_access_key_id = value.to_string(),
+        "secret_access_key" => aws_secret_access_key = value.to_string(),
+        "region" => region = Some(value.to_string()),
+        "output" => output = Some(value.to_string()),
+        _ => (),
+      }
+    }
+  }
 
-  // println!("profile_name: {}", first_match_or(&raw, &profile_name_regex, "default"));
-  // println!("aws_access_key_id: {}", first_match_or(&raw, &profile_name_regex, ""));
-  // println!("aws_secret_access_key: {}", first_match_or(&raw, &aws_secret_access_key_regex, ""));
-  // println!("region: {}", first_match_or(&raw, &region_regex, &template.region));
-  // println!("output: {}", first_match_or(&raw, &output_regex, &template.output));
-
-
-  let credential = AWSCredential {
-    profile_name: first_match_or(&raw, &profile_name_regex, ""),
-    aws_access_key_id: first_match_or(&raw, &profile_name_regex, ""),
-    aws_secret_access_key: first_match_or(&raw, &aws_secret_access_key_regex, ""),
-    region: first_match_or(&raw, &region_regex, &template.region),
-    output: first_match_or(&raw, &output_regex, &template.output),
-  };
-
-  credential
-}
-
-fn first_match_or(raw: &String, regex: &Regex, default: &str) -> String {
-  raw.split(regex).collect::<Vec<&str>>().first().unwrap_or(&default).to_string()
+  AWSCredential {
+    profile_name: sec,
+    aws_access_key_id: aws_access_key_id,
+    aws_secret_access_key: aws_secret_access_key,
+    region: region.unwrap_or_else(|| "eu-central-1".to_string()).to_string(),
+    output: output.unwrap_or_else(|| "json".to_string()).to_string(),
+  }
 }
