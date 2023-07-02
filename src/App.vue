c5387db2df79e19b58f37725059863d0f798548a
morozRed <moroz.grigory@gmail.com>
2023-07-03 00:10:16 +0200

    split into components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="min-h-screen bg-gray-800 text-white">
-    <img alt="Vue logo" src="./assets/logo.png" height="50" width="50">
-    <CredentialsList msg="Credentials List"/>
+  <div class="min-h-screen bg-gray-800 text-white p-5">
+    <div class="flex justify-center">
+      <img class="p-2" src="./assets/logo.png" height="50" width="50">
+      <h1 class="p-1 flex-auto text-3xl font-bold font-mono">rds_manager</h1>
+      <ProfilesList @profile-changed="updateSelectedProfile"/>
+    </div>
+    <InstancesList :selectedProfile="selectedProfile"/>
   </div>
 </template>
 
 <script>
-import CredentialsList from './components/CredentialsList.vue'
+  import InstancesList from './components/InstancesList.vue';
+  import ProfilesList from './components/ProfilesList.vue';
 
-export default {
-  name: 'App',
-  components: {
-    CredentialsList
+  export default {
+    name: 'App',
+    data() {
+      return {
+        selectedProfile: null,
+      };
+    },
+    methods: {
+      updateSelectedProfile(newProfile) {
+        this.selectedProfile = newProfile;
+      },
+    },
+    components: {
+      InstancesList,
+      ProfilesList
+    },
   }
-}
 </script>
