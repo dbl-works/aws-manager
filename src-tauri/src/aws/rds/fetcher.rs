0fd25a33a09f8941f0a21333c4815a778adff45d
Lud <lud@reinmiedl.com>
2024-01-13 13:46:49 +0100

    use correct username for database

--- src-tauri/src/aws/rds/fetcher.rs
+++ src-tauri/src/aws/rds/fetcher.rs
@@ -12,6 +12,11 @@ pub async fn get_instances() -> Result<Vec<RDSInstance>, Error> {
     let db_instances_slice = result.db_instances();
 
     let instances: Vec<RDSInstance> = db_instances_slice.iter().map(|db_instance| {
+        let identifier = db_instance.db_instance_identifier().unwrap_or_default().to_string();
+        let project = identifier.split("-").next().unwrap_or_default().to_string();
+        let environment = identifier.split("-").nth(1).unwrap_or_default().to_string();
+        let username = format!("{}_{}_readonly", project, environment);
+
         RDSInstance {
             endpoint: db_instance.endpoint().and_then(|e| e.address()).unwrap_or_default().to_string(),
             engine: db_instance.engine().unwrap_or_default().to_string(),
@@ -19,7 +24,7 @@ pub async fn get_instances() -> Result<Vec<RDSInstance>, Error> {
             name: db_instance.db_name().unwrap_or_default().to_string(),
             port: db_instance.endpoint().map_or(0, |e| e.port().unwrap_or(0)),
             status: db_instance.db_instance_status().unwrap_or_default().to_string(),
-            username: db_instance.master_username().unwrap_or_default().to_string(),
+            username: username,
         }
     }).collect();
 
